9dc3bd4f77c7f6446a5b740d06a860f8170ab395
Kasper de Graaff <kasperdegraaff1997@gmail.com>
2021-11-29 13:33:08 +0100

    added test

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -4,12 +4,14 @@
   import { defaultChainStore, web3, connected } from "svelte-web3"
 
   let storageValue = 0
-  let accounts,contract
+  let accounts, contract
 
   onMount(async () => {
-    // add a test to return in SSR context
+    // use the svelte-web3 library to connect to web3
     await defaultChainStore.setBrowserProvider()
+    // get the contract instance
     await getContract()
+    // run the contract example
     runExample()
   })
 
@@ -24,7 +26,6 @@
       SimpleStorage.abi,
       deployedNetwork && deployedNetwork.address
     )
-
   }
 
   async function runExample() {
@@ -34,6 +35,7 @@
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call()
 
+    // set the storageValue
     storageValue = response
   }
 </script>
@@ -41,27 +43,21 @@
 {#if !$connected}
   <div>Loading Web3, accounts, and contract...</div>
 {:else}
-<div class="container">
-  <div class="row">
+  <div class="box">
     <div class="col-8 offset-2">
       <h1 class="text-center text-dark">Truffle Box</h1>
-      <h4 class="text-center text-primary">Skeleton SvelteJS truffle box</h4>
+      <h2 class="text-center text-primary">Skeleton SvelteKit truffle box</h2>
 
-      <!-- <Information connected={connected}></Information> -->
-
-      <div class="alert alert-secondary">
-        <h4>Smart Contract Example</h4>
+        <h3>Smart Contract Example</h3>
         <p>
           If your contracts compiled and migrated successfully, below will show
           a stored value of 5 (by default).
         </p>
         <p>
-          Try changing the value stored on <strong>line 71</strong> of
-          <em>src/components/App.html</em>.
+          Try changing the value stored on <strong>line 33</strong> of 
+          <em>./client/src/routes/index.svelte</em>
         </p>
         <p>The stored value is: {storageValue}</p>
       </div>
-    </div>
   </div>
-</div>
 {/if}
